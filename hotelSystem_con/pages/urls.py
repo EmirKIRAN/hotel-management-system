1ee25a1cff892246028c88d170a9c80486a67825
EmirKIRAN <emirr.kirann@gmail.com>
2021-04-27 16:08:25 +0300

    added other pages and created partials

--- hotelSystem_con/pages/urls.py
+++ hotelSystem_con/pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('hotels/', views.hotels, name="hotels"),
+    path('roomlist/', views.room_list, name="room_list"),
+    path('room/', views.room_detail, name="room"),
 ]
--- hotelSystem_con/pages/views.py
+++ hotelSystem_con/pages/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def hotels(request):
+    return render(request, 'hotels.html')
+
+def room_list(request):
+    return render(request, 'room_list.html')
+
+def room_detail(request):
+    return render(request, 'room.html')
