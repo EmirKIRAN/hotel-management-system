2e696d19a9ecdf61aaeb829c11315711fbded1e6
EmirKIRAN <emirr.kirann@gmail.com>
2021-04-30 19:43:33 +0300

    created hotel model in project

---
+++ hotelSystem_con/hotels/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=255,verbose_name='Otel ismi')
+    owner = models.ForeignKey('auth.User', on_delete=models.SET(get_sentinel_user), verbose_name='Otel sahibi')
+    description = models.TextField(blank=True, null=True, verbose_name='Otel açıklaması')
+    image = models.ImageField(upload_to="hotels/")
+    address = models.CharField(max_length=255,verbose_name='Adres')
+    country = models.CharField(max_length=50,verbose_name='Ülkte')
+    city = models.CharField(max_length=50, verbose_name='Şehir')
+
+    def __str__(self):
+        return self.name
+
+
+
