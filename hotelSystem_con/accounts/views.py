b5f4b50ede005972f267ee0d0bac9455e9ff31f6
EmirKIRAN <emirr.kirann@gmail.com>
2021-05-03 02:55:24 +0300

    complited the login and register areas

--- hotelSystem_con/accounts/views.py
+++ hotelSystem_con/accounts/views.py
@@ -1,7 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def user_login(request):
-    return render(request, 'login.html')
+
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request=request, username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('dashboard')
+            else:
+                messages.warning(request, 'Disabled accounts')
+        else:
+            messages.info(request, 'Check your username or password')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login.html', {'form':form})
+
+
+
 
 def user_register(request):
-    return render(request, 'register.html')
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account has been created !')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html',{'form':form})
+
+
+def user_dashboard(request):
+
+    context = {
+        'title' : 'my dashboard',
+    }
+    return render(request, 'owner_index.html', context=context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
